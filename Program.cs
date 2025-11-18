0bfb7f10901503541ada080a301b2b1c3b52d7fe
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-11-18 07:52:20 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//*****************************************************************************
+//** 717. 1-bit and 2-bit Characters                                leetcode **
+//*****************************************************************************
+//** A trail of bits, where ones step two and zeros glide one,               **
+//** We walk the path to find if the final light is done.                    **
+//** If we land on the last lone spark, the truth shines clear and bright    **
+//** A single-bit character standing softly in the night.                    **
+//*****************************************************************************
+
+bool isOneBitCharacter(int* bits, int bitsSize) {
+    if(bitsSize == 1)
+    {
+        return true;
+    }
+
+    int i = 0;
+
+    while(1)
+    {
+        if(bits[i] == 0)
+        {
+            i += 1;
+        }
+        else
+        {
+            i += 2;
+        }
+
+        if(i == bitsSize - 1)
+        {
+            return true;
+        }
+        else if(i == bitsSize)
+        {
+            return false;
+        }
+    }
+}
